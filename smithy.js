be5aec8a94e2defb7625d7c4eb0f387d9ee4d646
Kai Tamkun <ktamkun@ucsc.edu>
2025-01-01 19:55:09 -0800

    Optimization options

--- smithy.js
+++ smithy.js
@@ -7,9 +7,13 @@ import fs from "fs";
 const {values, positionals} = parseArgs({
 	args: Bun.argv,
 	options: {
-		lang: {
-			type: "string",
-		},
+		lang: {type: "string"},
+		optimization: {type: "string"},
+		O0: {type: "boolean"},
+		O1: {type: "boolean"},
+		O2: {type: "boolean"},
+		O3: {type: "boolean"},
+		Ofast: {type: "boolean"},
 	},
 	strict: true,
 	allowPositionals: true,
@@ -22,6 +26,10 @@ const action = positionals[0];
 if (!action) {
 	console.error("Please specify an action.");
 	process.exit(1);
+}
+
+function done() {
+	process.exit(0);
 }
 
 function getLang() {
@@ -40,6 +48,20 @@ function getLangFlags(lang) {
 	return [];
 }
 
+function getOptimization() {
+	if (values.optimization) {
+		return values.optimization.trim().split(/\s+/);
+	}
+
+	for (const key of ["O0", "O1", "O2", "O3", "Ofast"]) {
+		if (values[key]) {
+			return [`-${key}`];
+		}
+	}
+
+	return ["-O0"];
+}
+
 if (action == "gen") {
 	const lang = getLang();
 	let i = 0;
@@ -51,6 +73,7 @@ if (action == "gen") {
 	const filename = `${i}.${lang}`;
 	await $`csmith ${getLangFlags(lang)} --seed ${i} -o ${filename}`;
 	console.log(`Generated ${filename}`);
+	done();
 }
 
 if (action == "verify") {
@@ -73,6 +96,12 @@ if (action == "verify") {
 
 	console.log(`Native checksum: \x1b[32m${nativeChecksum}\x1b[39m`);
 	const ll = `${i}.ll`;
-	await $`clang++ --std=c++20 -S -emit-llvm -Wno-everything ${filename} -o ${ll}`;
-	await $`ll2w ${ll}`;
+	const optimization = getOptimization();
+	await $`clang++ --std=c++20 -disable-O0-optnone -target mips64el-linux-gnu -S -emit-llvm -Wno-everything ${optimization} ${filename} -o ${ll}`;
+	try {
+		await $`ll2w ${ll}`;
+	} catch (err) {
+		console.log(err.stderr.toString());
+	}
+	done();
 }
